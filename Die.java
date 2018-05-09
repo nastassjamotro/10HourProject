9c47b79f99b1d7a8f13e6bb8d685f29d27906550
Nastassja Motro <nastassjamotro7@gmail.com>
2018-05-08 19:52:09 -0600

    Update Die.java

--- Die.java
+++ Die.java
@@ -1,1 +1,61 @@
 // Nastassja Motro 5/8/18
+
+import java.util.Random;
+
+public class Die {
+  
+  private static Random numberGenerator = new Random(); // ourRandNumGen
+  private final int mySide; // iMyNumSides
+  private int result; // iMyResult
+  public static final int SIDES = 6; //DEFAULT_SIDES
+  
+  public Die() {
+    this(SIDES);
+  }
+  
+  public Die(int numberOfSides) {
+    assert numberOfSides > 1: "Violation of condition: numberOfSides = " + numberOfSides + "numberOfSides must be greather than 1";
+    mySide = numberOfSides;
+    result = 1;
+    assert getResult() == 1 && getNumberOfSides() == numberOfSides;
+  }
+  
+  public Die(int numberOfSides, int endResult) {
+    assert numberOfSides > 1 && 1 <= endResult && endResult <= numberofSides: "Violation of condition";
+    mySide = numberOfSides;
+    result = endResult;
+  }
+  
+  public int roll() {
+    result = numberGenerator.nextInt(mySide) + 1;
+    assert( 1 <= getResult()) && (getResult() <= getNumberOfSides());
+    return result;
+  }
+  
+  public int getNumberOfSides() {
+    return mySide
+  }
+  
+  public int getResult() {
+    return result;
+  }
+  
+  public boolean equals(Object otherObject) {
+    boolean endResult = true;
+    if(otherObject == null) {
+      endResult = false;
+    } else if(this == otherObject) {
+      endResult = true;
+    } else if (this.getClass() != otherObject.getClass()) {
+      endResult = false;
+    } else {
+      Die otherDie = (Die)otherObject;
+      endResult = this.result == otherDie.result && this.mySide == otherDie.mySide;
+    }
+    return endResult;
+  }
+  
+  public String toString() {
+    return "Number of Sides " + getNumberOfSides() + " result " + getResult();
+  }
+}
